a315fb631a5a2acfd4ee6a9b0c1fed0e6f121826
Michel Müller <michel@typhooncomputing.com>
2015-01-27 14:51:54 +0900

    disabled mandatory valgrind run, fixed comments inserted by preprocessor that would confuse fortran debuggers

--- hf_bin/runTests.sh
+++ hf_bin/runTests.sh
@@ -36,10 +36,8 @@ for executable in $executables; do
 		exit $rc
 	fi
 	validation_rc=${rc}
-	if [ "$run_mode" = "debug" ]; then
-		if [[ $validation_rc != 0 ]] ; then
-			echo "There was an error during the validation run. Since we're in debug mode, repeating the test with valgrind running. This might take a while."
-		fi
+	if [[ "$run_mode" = "debug" ]] && [[ $validation_rc != 0 ]]; then
+		echo "There was an error during the validation run. Since we're in debug mode, repeating the test with valgrind running. This might take a while."
 		${HF_DIR}/hf_bin/runTest.sh $executableName $architecture valgrind "$output_file_pattern" $source_before $source_after
 		rc=$?
 		if [[ $rc != 0 ]] ; then

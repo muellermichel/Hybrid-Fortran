9616ee62a23bcfa2c33c918220da64b6aeed4f67
Michel Müller <michel@typhooncomputing.com>
2016-05-31 16:55:07 +0900

    fixed a problem with generic symbol domain names in conjunction with region position adjustments with the new simplified domain loading code.

--- hf/models/symbol.py
+++ hf/models/symbol.py
@@ -845,11 +845,24 @@ EXAMPLE:\n\
 				self.domains
 			))
 		if self.initLevel >= Init.DECLARATION_LOADED \
+		and self.parallelRegionPosition in ["within", "inside"] \
 		and self.isAutoDom \
 		and len(self.domains) not in [
 			len(self._templateDomains),
 			len(self._templateDomains) + len(self._declaredDimensionSizes)
 		]:
+			raise Exception("Wrong number of domains for autoDom symbol %s: || template: %s; || declared: %s || actual: %s || kernel: %s || non-kernel: %s" %(
+				self.name,
+				self._templateDomains,
+				self._declaredDimensionSizes,
+				self.domains,
+				self._kernelDomainNames,
+				self._kernelInactiveDomainSizes
+			))
+		if self.initLevel >= Init.DECLARATION_LOADED \
+		and self.parallelRegionPosition in ["outside", None, ""] \
+		and self.isAutoDom \
+		and len(self.domains) != len(self._kernelInactiveDomainSizes):
 			raise Exception("Wrong number of domains for autoDom symbol %s: || template: %s; || declared: %s || actual: %s || kernel: %s || non-kernel: %s" %(
 				self.name,
 				self._templateDomains,
@@ -873,16 +886,16 @@ EXAMPLE:\n\
 		self.loadDeclarationPrefixFromString(declarationPrefixFromTemplate)
 		self.loadDomains(templateDomains, parallelRegionTemplates)
 		logging.debug(
-				"[" + str(self) + ".init " + str(self.initLevel) + "] Domains loaded from callgraph information for symbol %s. Parallel active: %s. Parallel Inactive: %s. Declaration Prefix: %s. templateDomains: %s declarationPrefix: %s. Parallel Regions: %i\n" %(
-					str(self),
-					str(self._kernelDomainNames),
-					str(self._kernelInactiveDomainSizes),
-					declarationPrefixFromTemplate,
-					templateDomains,
-					declarationPrefixFromTemplate,
-					len(parallelRegionTemplates)
-				)
+			"[" + str(self) + ".init " + str(self.initLevel) + "] Domains loaded from callgraph information for symbol %s. Parallel active: %s. Parallel Inactive: %s. Declaration Prefix: %s. templateDomains: %s declarationPrefix: %s. Parallel Regions: %i\n" %(
+				str(self),
+				str(self._kernelDomainNames),
+				str(self._kernelInactiveDomainSizes),
+				declarationPrefixFromTemplate,
+				templateDomains,
+				declarationPrefixFromTemplate,
+				len(parallelRegionTemplates)
 			)
+		)
 
 	def loadDeclarationPrefixFromString(self, declarationPrefix):
 		if declarationPrefix != None and declarationPrefix.strip() != "":
@@ -913,7 +926,7 @@ EXAMPLE:\n\
 			self.domains = templateDomains
 
 		#   which of those dimensions are kernel dimensions?
-		#   -> put them in the 'parallelActive' set, .....
+		#   -> build up index of domain sizes and and names and put them in the 'parallelActive' set, .....
 		parallelRegionDomNamesBySize = {}
 		for parallelRegionTemplate in parallelRegionTemplates:
 			regionDomNameAndSize = getDomNameAndSize(parallelRegionTemplate)
@@ -936,7 +949,7 @@ EXAMPLE:\n\
 					self._knownKernelDomainSizesByName[regionDomName].append(regionDomSize)
 				parallelRegionDomNamesBySize[regionDomSize] = regionDomName
 
-		#   .... put the others in the 'parallelInactive' set.
+		#   .... build up index of domain sizes and and names and put the others in the 'parallelInactive' set.
 		for (dependantDomName, dependantDomSize) in self.domains:
 			#build up parallel inactive dimensions again
 			if dependantDomName not in self._kernelDomainNames \
@@ -947,6 +960,17 @@ EXAMPLE:\n\
 			if domNameAlias in self._knownKernelDomainSizesByName \
 			and dependantDomSize not in self._knownKernelDomainSizesByName[domNameAlias]:
 				self._knownKernelDomainSizesByName[domNameAlias].append(dependantDomSize)
+
+		#   match the domain sizes to those in the index. this is important so we don't cancel them out later in the region position adjustment code
+		dimsBeforeReset = copy.deepcopy(self.domains)
+		self.domains = []
+		for (dependantDomName, dependantDomSize) in dimsBeforeReset:
+			domNameAlias = parallelRegionDomNamesBySize.get(dependantDomSize, dependantDomName)
+			if domNameAlias not in self._kernelDomainNames \
+			and dependantDomSize not in self._kernelInactiveDomainSizes:
+				raise Exception("Symbol %s's dependant domain size %s (domain %s / %s) is not declared as one of its dimensions. Parallel Active dims: %s; Inactive: %s" \
+					%(self.name, dependantDomSize, dependantDomName, domNameAlias, self._kernelDomainNames, self._kernelInactiveDomainSizes))
+			self.domains.append((domNameAlias, dependantDomSize))
 
 		# logging.debug("[" + self.name + ".init " + str(self.initLevel) + "] before reset. parallel active: %s; parallel inactive: %s" %(
 		# 		str(self._kernelDomainNames),
@@ -1089,6 +1113,7 @@ EXAMPLE:\n\
 		self.declarationSuffix = remainder.strip()
 		dimensionSizes = [sizeStr.strip() for sizeStr in dimensionStr.split(',') if sizeStr.strip() != ""] if dimensionStr != None else []
 		self._declaredDimensionSizes = dimensionSizes
+
 		knownDimensionSizes = [d for (_, d) in self.domains]
 		if self.isAutoDom and self.hasUndecidedDomainSizes:
 			if len(self.domains) == 0:

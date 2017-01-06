cd4aa5106ba8f24797d865c80407b06b079a5103
Michel Müller <michel@typhooncomputing.com>
2017-01-06 19:01:33 +0900

    cleaned up ordered objects script

--- hf_bin/object_files_ordered.py
+++ hf_bin/object_files_ordered.py
@@ -137,56 +137,13 @@ def change_extension(file, build='', ext=".o"):
     return os.path.join(build, file.split(".")[0]) +ext
 
 #Definitions
-def run(files=None,verbose=True,overwrite=None,output=None,macros={},build=''):
-
-    l=create_file_objs(files,macros)
+def run(verbose=True, build=''):
+    l=create_file_objs()
     mod2fil=file_objs_to_mod_dict(file_objs=l)
     depends=get_depends(fob=l,m2f=mod2fil)
 
-    print depends
-    for objfile in OrderedSet([change_extension(f) for f in walkTree(makeTree(depends))]):
-        print objfile
-
-    if verbose:
-        for i in depends.keys():
-            print "\033[032m"+i+"\033[039m depends on :\033[034m"
-            for j in depends[i]: print "\t"+j
-            print "\033[039m"
-
-    if output is None:
-        output = "makefile.dep"
-
-    tmp=write_depend(outfile=output,dep=depends,overwrite=overwrite,build=build)
-
-    return depends
-
-def write_depend(outfile="makefile.dep",dep=[],overwrite=False,build=''):
-    "Write the dependencies to outfile"
-    #Test file doesn't exist
-    if os.path.exists(outfile):
-        if not(overwrite):
-            print "\033[031mWarning file exists.\033[039m"
-            opt=raw_input("Overwrite? Y... for yes.")
-        else:
-            opt="y"
-        if opt.lower().startswith("y"):
-            pass
-        else:
-            return
-
-    #Open file
-    f=open(outfile,'w')
-    f.write('# This file is generated automatically. DO NOT EDIT!\n')
-    for i in dep.keys():
-        tmp,fil=os.path.split(i)
-        stri="\n"+change_extension(fil, build) + " : "
-        for j in dep[i]:
-            tmp,fil=os.path.split(j)
-            stri=stri+" \\\n\t"+change_extension(fil, build)
-        stri=stri+"\n"
-        f.write(stri)
-    f.close()
-    return
+    objFilesOrdered = OrderedSet([change_extension(f, build) for f in walkTree(makeTree(depends))])
+    print " ".join(objFilesOrdered)
 
 def get_source(ext=[".f90",".F90",".h90",".H90",".P90"]):
     "Return all files ending with any of ext"
@@ -270,7 +227,8 @@ def get_depends(fob=[],m2f=[]):
             try:
                 tmp.append(m2f[j.lower()])
             except:
-                print "\033[031mError\033[039m module \033[032m"+j+"\033[039m not defined in any files. Skipping..."
+                pass
+                # print "\033[031mError\033[039m module \033[032m"+j+"\033[039m not defined in any files. Skipping..."
 
         deps[i.file_name]=tmp
 
@@ -290,26 +248,12 @@ if __name__ == "__main__":
 
     # Add command line arguments
     parser = argparse.ArgumentParser(description='Generate Fortran dependencies')
-    parser.add_argument('-f','--files',nargs='+',help='Files to process')
-    parser.add_argument('-D',nargs='+',action='append',metavar='NAME=DESCRIPTION',
-                        help="""The macro NAME is replaced by DEFINITION in 'use' statements""")
     parser.add_argument('-b','--build',nargs=1,help='Build Directory (prepended to all files in output',
                         default='')
-    parser.add_argument('-o','--output',nargs=1,help='Output file')
     parser.add_argument('-v','--verbose',action='store_true',help='explain what is done')
 
     # Parse the command line arguments
     args = parser.parse_args()
-
-    # Assemble a dictionary out of the macro definitions
-    macros = {}
-    if args.D:
-        for arg in args.D:
-            for var in arg:
-                temp = var.split('=')
-            macros[temp[0]] = temp[1]
-
-    output = args.output[0] if args.output else None
     build = args.build[0] if args.build else ''
 
-    run(files=args.files, verbose=args.verbose, overwrite=True, macros=macros, output=output, build=build)
+    run(verbose=args.verbose, build=build)
